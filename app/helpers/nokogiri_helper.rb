0c2edb9107bd39065265cc54a83ef1552796c23f
Gao Yun <gao_yun@qq.com>
2016-05-28 20:31:11 +0000

    adjust reg and login form

--- app/helpers/nokogiri_helper.rb
+++ app/helpers/nokogiri_helper.rb
@@ -4,4 +4,5 @@ module NokogiriHelper
 		response=Net::HTTP.get_response(URI('http://route.showapi.com/105-31?showapi_appid=18266&showapi_sign=0142ab2f164a43999ddd4fff43ebd063&fromCode=JPY&toCode=CNY&money=100&'))
 		$rate = JSON.parse(response.body)['showapi_res_body']['money']
 	  #end
+
 end
