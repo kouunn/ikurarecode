0fe85327d3229a2c3f2641b26052bd2fcb831126
Gao Yun <gao_yun@qq.com>
2016-06-02 15:12:11 +0000

    add baidu翻译

--- app/controllers/nokogiri_controller.rb
+++ app/controllers/nokogiri_controller.rb
@@ -29,6 +29,8 @@ class NokogiriController < ApplicationController
                   
               end
 
+        #搜索关键字转日文
+        #@key_word = zh2jp(@key_word)
 
     		@kproducts = Hash.new
     		@aproducts = Hash.new
@@ -88,6 +90,18 @@ class NokogiriController < ApplicationController
     		end
   	end
 
+    def zh2jp(text)
+      md5 = Digest::MD5.hexdigest("20160527000022236#{text}ikura2016CPtF543nsRwPIleARxOf")
+      url="http://api.fanyi.baidu.com/api/trans/vip/translate?q=#{text}&from=zh&to=jp&appid=20160527000022236&salt=ikura2016&sign=#{md5}"
+      response=Net::HTTP.get_response(URI(URI.escape(url)))
+      JSON.parse(response.body)['trans_result'][0]['dst']
+    end
 
+    def jp2zh(text)
+      md5 = Digest::MD5.hexdigest("20160527000022236#{text}ikura2016CPtF543nsRwPIleARxOf")
+      url="http://api.fanyi.baidu.com/api/trans/vip/translate?q=#{text}&from=jp&zh=jp&appid=20160527000022236&salt=ikura2016&sign=#{md5}"
+      response=Net::HTTP.get_response(URI(URI.escape(url)))
+      JSON.parse(response.body)['trans_result'][0]['dst']
+    end
 
 end
--- app/helpers/nokogiri_helper.rb
+++ app/helpers/nokogiri_helper.rb
@@ -5,4 +5,17 @@ module NokogiriHelper
 		$rate = JSON.parse(response.body)['showapi_res_body']['money']
 	  #end
 
+	  def zh2jp(text)
+	    md5 = Digest::MD5.hexdigest("20160527000022236#{text}ikura2016CPtF543nsRwPIleARxOf")
+	    url="http://api.fanyi.baidu.com/api/trans/vip/translate?q=#{text}&from=zh&to=jp&appid=20160527000022236&salt=ikura2016&sign=#{md5}"
+	    response=Net::HTTP.get_response(URI(URI.escape(url)))
+	    JSON.parse(response.body)['trans_result'][0]['dst']
+	  end
+
+	  def jp2zh(text)
+	    md5 = Digest::MD5.hexdigest("20160527000022236#{text}ikura2016CPtF543nsRwPIleARxOf")
+	    url="http://api.fanyi.baidu.com/api/trans/vip/translate?q=#{text}&from=jp&zh=jp&appid=20160527000022236&salt=ikura2016&sign=#{md5}"
+	    response=Net::HTTP.get_response(URI(URI.escape(url)))
+	    JSON.parse(response.body)['trans_result'][0]['dst']
+	  end
 end
